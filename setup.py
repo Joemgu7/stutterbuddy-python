f74f8a490d804102c68fca09f3f518507625e927
Joemgu7 <briguetjo@yahoo.de>
2023-01-05 18:40:24 +0100

    fix imports

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stutterbuddy",
-    version="0.2.1",
+    version="0.2.2",
     author="Jonas Briguet",
     author_email="briguetjo@yahoo.de",
     description="Automate video editing with Stutterbuddy",
--- stutterbuddy/__init__.py
+++ stutterbuddy/__init__.py
@@ -2,10 +2,10 @@
     Author: Jonas Briguet
     Date: 06.12.2021
     Last Change: 3.01.2023
-    version: 0.2.0
+    version: 0.2.2
     This is a library with functions to interact with the stutterbuddy.ch api 
 """
 
-from lib import *
+from .lib import Stutterbuddy, Asset, Job, SubmissionSettings
 
 
